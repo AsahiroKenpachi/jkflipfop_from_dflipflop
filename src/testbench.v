603e226d7c21a644791851da9ae5d48578308c03
AsahiroKenpachi <137492506+AsahiroKenpachi@users.noreply.github.com>
2024-01-10 10:31:53 +0530

    Update testbench.v

--- src/testbench.v
+++ src/testbench.v
@@ -1,8 +1,9 @@
 `include"jkfromd.v"
 module tb();
   wire Q;
-  reg clk,din,rst;
-  top_module t1(clk,din,rst,Q);
+  reg clk,j,k,rst;
+  top_module t1(clk,j,k,rst,Q);
+  integer count1,count2;
   initial begin
     clk=0;
     forever begin
@@ -14,5 +15,20 @@ module tb();
     rst=1;
     #2;
     rst=0;
-    
+    j=0;
+    k=0
+    for(count1=0;count1<2;count1=count1+1)
+      begin
+        for(count2=0;count2<2;count2=count2+1) begin
+          #2;
+          k=k+1;
+        end
+        j=j+1;
+      end
+    $finish();
   end
+  initial begin
+    $dumpfile("signal.vcd");
+    $dumpvars(0,tb);
+  end
+endmodule
